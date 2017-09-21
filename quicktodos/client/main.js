8ce55e1878bc895f63be052dcd3a96499e0cbe8d
rubik951 <rubik.junk@gmail.com>
2017-09-21 18:25:34 +0300

    Finished project

--- quicktodos/client/main.js
+++ quicktodos/client/main.js
@@ -1,16 +1,52 @@
-import { Template } from 'meteor/templating';
+import { Template } from "meteor/templating";
+import { Accounts } from "meteor/accounts-base";
 
-const todos = [
-  {text: 'Pickup kids from school'},
-  {text: 'Go food shopping'},
-  {text: 'Meeting with boss'}
-];
+Accounts.ui.config({
+  passwordSignupFields: "USERNAME_ONLY"
+});
+
+Template.main.onCreated(function mainOnCreated() {
+  Meteor.subscribe("todos");
+});
 
 Template.main.helpers({
-  title(){
-    return 'Quick Todos';
+  title() {
+    return "Quick Todos";
   },
-  todos(){
+  todos() {
+    const todos = Todos.find();
     return todos;
+  }
+});
+
+Template.main.events({
+  "submit .add-todo"(event) {
+    event.preventDefault();
+
+    const text = event.target.text.value;
+    const time = event.target.time.value;
+
+    Meteor.call("todos.insert", text, time);
+
+    event.target.text.value = "";
+    event.target.time.value = "";
+  }
+});
+
+Template.todo.events({
+  "click .toggle-checked"(event) {
+    Meteor.call("todos.setChecked", this._id, !this.checked);
+  },
+  "click .delete"(event) {
+    Meteor.call("todos.delete", this._id);
+  },
+  "click .toggle-private"() {
+    Meteor.call("todos.setPrivate", this._id, !this.private);
+  }
+});
+
+Template.todo.helpers({
+  isOwner() {
+    return this.owner === Meteor.userId();
   }
 });
--- quicktodos/server/main.js
+++ quicktodos/server/main.js
@@ -1,5 +1,33 @@
-import { Meteor } from 'meteor/meteor';
+import { Meteor } from "meteor/meteor";
 
-Meteor.startup(() => {
-  // code to run on server at startup
+Meteor.publish("todos", function todosPublication() {
+  return Todos.find({
+    $or: [{ private: { $ne: true } }, { owner: this.userId }]
+  });
+});
+
+Meteor.methods({
+  "todos.insert"(text, time) {
+    Todos.insert({
+      text,
+      time,
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+  "todos.setChecked"(id, setChecked) {
+    Todos.update(id, { $set: { checked: setChecked } });
+  },
+  "todos.delete"(id) {
+    Todos.remove(id);
+  },
+  "todos.setPrivate"(id, setToPrivate) {
+    const todo = Todos.findOne(id);
+
+    if (todo.owner !== this.userId) {
+      throw new Meteor.Error("Unauthorized");
+    }
+
+    Todos.update(id, { $set: { private: setToPrivate } });
+  }
 });
